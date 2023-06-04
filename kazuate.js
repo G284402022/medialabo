11dcb4d9bfe652c036e6b0f20e03f990f6e9928a
G284402022 <228440@st.takushoku-u.ac.jp>
2023-06-05 02:08:45 +0900

    修正

--- kazuate.js
+++ kazuate.js
@@ -14,6 +14,7 @@ let kaisu = 0;
 let h = document.querySelector('button#hantei');
 h.addEventListener('click', hantei);
 
+
 function hantei() {
   let i = document.querySelector('input[name="number"]');
   let number = i.value;
